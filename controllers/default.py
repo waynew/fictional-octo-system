13534167da695df073aa92cb48782258d82f2b5e
Wayne Werner <wayne@waynewerner.com>
2015-12-31 19:36:17 -0600

    Hey cool, I can post data!

--- controllers/default.py
+++ controllers/default.py
@@ -1,45 +1,34 @@
 # -*- coding: utf-8 -*-
 
 def index():
-    return dict(message='Cool')
-    return 'Halloooooo!'
-
-#
-#def user():
-#    """
-#    exposes:
-#    http://..../[app]/default/user/login
-#    http://..../[app]/default/user/logout
-#    http://..../[app]/default/user/register
-#    http://..../[app]/default/user/profile
-#    http://..../[app]/default/user/retrieve_password
-#    http://..../[app]/default/user/change_password
-#    http://..../[app]/default/user/bulk_register
-#    use @auth.requires_login()
-#        @auth.requires_membership('group name')
-#        @auth.requires_permission('read','table name',record_id)
-#    to decorate functions that need access control
-#    also notice there is http://..../[app]/appadmin/manage/auth to allow administrator to manage users
-#    """
-#    return dict(form=auth())
-#
-#
-#@cache.action()
-#def download():
-#    """
-#    allows downloading of uploaded files
-#    http://..../[app]/default/download/[filename]
-#    """
-#    return response.download(request, db)
-#
-#
-#def call():
-#    """
-#    exposes services. for example:
-#    http://..../[app]/default/call/jsonrpc
-#    decorate with @services.jsonrpc the functions to expose
-#    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
-#    """
-#    return service()
-#
-#
+    form = FORM(INPUT(_name='firstname'),
+                INPUT(_name='lastname'),
+                INPUT(_name='date_of_birth'),
+                INPUT(_name='zipcode'),
+                BUTTON('This is a button, is it not?', _type='submit'),
+                _action="save_info",
+               )
+    form = SQLFORM.factory(Field(
+                               'firstname',
+                               label='What should we call you?',
+                               requires=IS_NOT_EMPTY(),
+                           ),
+                           Field(
+                               'lastname',
+                               label='What is your family name?',
+                               requires=IS_NOT_EMPTY(),
+                           ),
+                           Field(
+                               'date_of_birth',
+                               label='Date of Birth',
+                               requires=IS_NOT_EMPTY(),
+                           ),
+                           Field(
+                               'zipcode',
+                               label='Zipcode',
+                               requires=IS_NOT_EMPTY(),
+                           ),
+    )
+    if form.process():
+        print(form.vars)
+    return dict(message='Cool', form=form)
